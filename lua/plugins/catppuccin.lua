11c456a4bba9c48c46da07615ff1d0b68493d174
mtikvica <mato.tikvica@gmail.com>
2025-01-28 16:21:51 +0100

    🔧 Added LSP

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,19 +1,19 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,  -- Ensure this theme loads early
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,  -- Ensure this theme loads early
 
-    config = function()
-        -- Optional setup configuration for Catppuccin theme
-        require("catppuccin").setup({
-            flavour = "macchiato",  -- Choose from "latte", "macchiato", "mocha", or "frappe"
-            background = {
-                light = "latte",     -- Light variant
-                dark = "mocha",      -- Dark variant
-            },
-        })
+  config = function()
+    -- Optional setup configuration for Catppuccin theme
+    require("catppuccin").setup({
+      flavour = "macchiato",  -- Choose from "latte", "macchiato", "mocha", or "frappe"
+      background = {
+        light = "latte",     -- Light variant
+        dark = "mocha",      -- Dark variant
+      },
+    })
 
-        -- Set the colorscheme to Catppuccin
-        vim.cmd('colorscheme catppuccin')
-    end
+    -- Set the colorscheme to Catppuccin
+    vim.cmd('colorscheme catppuccin')
+  end
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,31 @@
 return {
-  'nvim-telescope/telescope.nvim', 
-  tag = '0.1.8',
-  dependencies = { 
-    'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    config = function()
+      vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+      vim.keymap.set("n", "<space>en", function()
+        require('telescope.builtin').find_files{
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
+    end
   },
-  config = function()
-    vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
-    vim.keymap.set("n", "<space>en", function()
-      require('telescope.builtin').find_files{
-        cwd = vim.fn.stdpath("config")
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+          }
+        }
       }
-    end)
-  end
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
